fed35cb9ea38993514a98a080f0e3701b85e246e
Enesyilm <ensylmz1441@gmail.com>
2024-01-29 23:49:13 +0300

    initialization of project

---
+++ Alarmify/Alarmify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alarmify
+//
+//  Created by Enes Talha Yılmaz on 29.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
